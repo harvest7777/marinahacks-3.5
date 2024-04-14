9f0c2be30cb64cd7d6160ab5e060483d5ffd348e
harvest7777 <148602862+harvest7777@users.noreply.github.com>
2024-04-13 20:54:24 -0700

    made components

--- marinahacks/src/App.js
+++ marinahacks/src/App.js
@@ -1,24 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import UserButton from './username/userbutton';
-import MessageBox from './sendmessage/sendmessage';
-import DisplayMessage from './displaymessages/displaymessages';
+import UserButton from './components/userbutton';
+import MessageBox from './components/sendmessage';
+import DisplayMessage from './components/displaymessages';
 import './assets/baskic8.css'
 import './App.css';
 function App() {
-  // This is just an example of how you would access an API endpoint
-  // DO NOT actually access them all in app, it should be accessed within a react component this is just an example
-  const [data, setData] = useState();
-
-  useEffect(() => {
-    fetch("/test")
-      .then(res => res.json())
-      .then(data => {
-        setData(data);
-        console.log(data);
-      });
-  }, []);
-  // DO NOT ACCESS API ENDPOINTS IN APP.JS THIS IS JUST AN EXAMPLE
-
   return (
     // We will be putting all our react components here
     <div>
