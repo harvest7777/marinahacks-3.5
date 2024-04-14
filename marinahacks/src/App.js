609ca56219ff81d6880e8f040d2bac0cc589d328
sickk <jaakmyr@gmail.com>
2024-04-14 01:25:27 -0700

    error ass push

--- marinahacks/src/App.js
+++ marinahacks/src/App.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import ChatRoom from './components/ChatRoom';
 import ColorSelector from './components/ColorSelector';
-import UserButton from './username/userbutton';
-import MessageBox from './sendmessage/sendmessage';
-import DisplayMessage from './displaymessages/displaymessages';
+import UserButton from './components/userbutton';
+import MessageBox from './components/sendmessage';
+import DisplayMessage from './components/displaymessages';
 import MainUserSection from './components/MainUserSection';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './assets/baskic8.css'
@@ -23,8 +23,6 @@ function App() {
   }, []);
   // DO NOT ACCESS API ENDPOINTS IN APP.JS THIS IS JUST AN EXAMPLE
     
-
-function App() {
   return (
     // We will be putting all our react components here
     <div>
